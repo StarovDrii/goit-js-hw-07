75a04fdccf92dc17ef9cb0a4b5c396077b8acf12
StarovDrii <131916457+StarovDrii@users.noreply.github.com>
2023-12-24 16:36:58 +0200

    task 3 fix

--- js/task-03.js
+++ js/task-03.js
@@ -1,8 +1,7 @@
 const input = document.querySelector('#name-input');
 const span = document.querySelector('#name-output');
 input.addEventListener('input', () => {
-  span.textContent = input.value;
-  if (input.value === '' || input.value.startsWith(' ')) {
-    span.textContent = 'Anonymous';
-  }
+  const inputValue = input.value.trim();
+  console.log(inputValue);
+  span.textContent = inputValue ? inputValue : 'Anonymous';
 });
